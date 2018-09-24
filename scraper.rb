5da767a2867db65e3d632422b683a204de772f0c
Noel Delgado <noeldelgadom@gmail.com>
2018-09-24 13:17:22 -0500

    initial get with HTTParty

--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,11 @@ require 'httparty'
 require 'byebug'
 
 def scraper
-  puts 'Im in here'
+  url           = "https://www.linkedin.com/jobs/search?keywords=&location=Mexico%20City%20Area%2C%20Mexico&locationId=mx:5921"
+	unparsed_page = HTTParty.get(url)
+  parsed_page   = Nokogiri::HTML(unparsed_page)
+
+  byebug
 end
 
 scraper

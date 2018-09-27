f50dfa59cc6b57f421a3747b38b347ae10104342
Noel Delgado <noeldelgadom@gmail.com>
2018-09-27 17:11:45 -0500

    extracted job title, company and poster including their urls

--- scraper.rb
+++ scraper.rb
@@ -24,24 +24,39 @@ def login(browser)
   return browser
 end
 
-def extract_companies(browser)
+def extract_jobs(browser)
   job_page = 'https://www.linkedin.com/jobs/search/?location=Mexico%20City%20Area%2C%20Mexico&locationId=mx%3A5921'
   browser.goto(job_page)
 
-  companies = []
+  jobs = []
   browser.ul(class: 'jobs-search-results__list').children.each do |li|
     li.click
-    company = {}
-    company[:name] = browser.a(class: 'jobs-details-top-card__company-url').inner_text
-    company[:link] = browser.a(class: 'jobs-details-top-card__company-url').href
-    companies << company
+    sleep(1)
+
+    if browser.div(class: 'jobs-poster').exists?  # Not all job posts have job-posters, hence this if statement
+      puts 'poster exists'
+      job = {}
+
+      job[:job_name]          = browser.a(class: 'jobs-details-top-card__job-title-link').inner_text
+      job[:job_url]           = browser.a(class: 'jobs-details-top-card__job-title-link').href
+
+      job[:company_name]      = browser.a(class: 'jobs-details-top-card__company-url').inner_text
+      job[:company_url]       = browser.a(class: 'jobs-details-top-card__company-url').href
+
+      job[:job_poster_name]   = browser.p(class: 'jobs-poster__name').inner_text
+      job[:job_poster_url]    = browser.div(class: 'jobs-poster__wrapper').a.href
+      
+      jobs << job
+    else
+      puts 'poster doesnt exist'
+    end
   end
 
   # Need to add pagination. As of now only 1st page is processed
 
   byebug
-
-  return companies
+  
+  return jobs
 end
 
 def scraper
@@ -49,7 +64,7 @@ def scraper
   
   browser = login(browser)
 
-  companies = extract_companies(browser)
+  jobs = extract_jobs(browser)
 end
 
 scraper

8478f47a936cb20ea6fb443e2dbcd7f8d994e5e8
Noel Delgado <noeldelgadom@gmail.com>
2018-09-27 14:55:38 -0500

    added job_page link

--- scraper.rb
+++ scraper.rb
@@ -24,12 +24,29 @@ def login(browser)
   return browser
 end
 
+def extract_companies(browser)
+  companies = []
+
+  company = {}
+  company[:name] = "sample company"
+  company[:link] = "sample link"
+  companies << company
+
+  job_page = 'https://www.linkedin.com/jobs/search/?location=Mexico%20City%20Area%2C%20Mexico&locationId=mx%3A5921'
+
+  browser.goto(job_page)
+
+  byebug
+
+  return companies
+end
+
 def scraper
   browser = Watir::Browser.new
   
   browser = login(browser)
-  byebug
 
+  companies = extract_companies(browser)
 end
 
 scraper

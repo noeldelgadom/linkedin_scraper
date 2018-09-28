0394d2237fed400608760814928249dbd4229649
Noel Delgado <noeldelgadom@gmail.com>
2018-09-28 16:50:36 -0500

    passed more constants to google_sheets_constants

--- google_sheets_constants.rb
+++ google_sheets_constants.rb
@@ -1,5 +1,7 @@
-OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'.freeze
-APPLICATION_NAME = 'Google Sheets API Ruby Quickstart'.freeze
-CREDENTIALS_PATH = 'credentials.json'.freeze
-TOKEN_PATH = 'token.yaml'.freeze
-SCOPE = Google::Apis::SheetsV4::AUTH_SPREADSHEETS
+OOB_URI           = 'urn:ietf:wg:oauth:2.0:oob'.freeze
+APPLICATION_NAME  = 'Google Sheets API Ruby Quickstart'.freeze
+CREDENTIALS_PATH  = 'credentials.json'.freeze
+TOKEN_PATH        = 'token.yaml'.freeze
+SCOPE             = Google::Apis::SheetsV4::AUTH_SPREADSHEETS
+SPREADSHEET_ID    = '1R3sEe4NENwr9Su7hda7be9BZCGcCDTZ03lurXp0E2hk'
+RANGE             = 'Sheet1!A2:E'
--- scraper.rb
+++ scraper.rb
@@ -13,28 +13,26 @@ require_relative 'authorize.rb'
 require_relative 'easy_load_jobs.rb'
 
 def scraper
-  #Open Two Browsers. One scans the jobs, the other the companies and contacts
-  browser_one = Watir::Browser.new
-  browser_two = Watir::Browser.new
-  browser_one = login(browser_one)
-  browser_two = login(browser_two)
+  # Open Two Browsers. One scans the jobs, the other the companies and contacts
+  # browser_one = Watir::Browser.new
+  # browser_two = Watir::Browser.new
+  # browser_one = login(browser_one)
+  # browser_two = login(browser_two)
 
-  jobs = extract_jobs(browser_one, browser_two)       # Extracts real data from LinkedIn
-  # jobs = easy_load_jobs                             # Extracts fake data from easy_load_jobs.rb
+  # jobs = extract_jobs(browser_one, browser_two)       # Extracts real data from LinkedIn
+  jobs = easy_load_jobs                             # Extracts fake data from easy_load_jobs.rb
 
   google_array = []
   jobs.each {|job| google_array << job.values }
 
-  # Google Spreadsheet part
+  # Initiate Google Spreadsheets
   service = Google::Apis::SheetsV4::SheetsService.new
   service.client_options.application_name = APPLICATION_NAME
   service.authorization = authorize
 
-  spreadsheet_id = '1R3sEe4NENwr9Su7hda7be9BZCGcCDTZ03lurXp0E2hk'
-  range = 'Sheet1!A2:E'
   value_range = Google::Apis::SheetsV4::ValueRange.new(values: google_array)
-  result = service.append_spreadsheet_value(spreadsheet_id,
-                                            range,
+  result = service.append_spreadsheet_value(SPREADSHEET_ID,
+                                            RANGE,
                                             value_range,
                                             value_input_option: "USER_ENTERED")
   
